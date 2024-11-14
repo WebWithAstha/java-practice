ee2f87eca95364c811272debcda3b08cc2b04222
WebWithAstha <lodhiastha85@gmail.com>
2024-11-14 23:22:46 +0530

    checking valid voter

--- src/control_flow/Program1.java
+++ src/control_flow/Program1.java
@@ -1,4 +1,21 @@
 package control_flow;
 
+import java.util.Scanner;
+
+// check user's voter validity
 public class Program1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if( age > - 1 &&  age < 151){
+            if(age > 18){
+                System.out.println("Valid voter");
+            }else{
+                System.out.println("You are not a voter.");
+                System.out.println("You will be able to vote in " + (18-age) + (((18-age) > 1)?" years.":" year."));
+            }
+        }else{
+            System.out.println("Invalid age.");
+        }
+    }
 }
