deeb5f132eb95a116f08c276126bc7e77ba4f24e
WebWithAstha <lodhiastha85@gmail.com>
2024-12-02 20:43:54 +0530

    split the string

---
+++ src/strings/Program6_SplitString.java
@@ -0,0 +1,14 @@
+package strings;
+
+import java.util.Arrays;
+
+public class Program6_SplitString {
+
+    public static void main(String[] args) {
+        String s = "aaj gya devraj shaadi me";
+        String[] ans = s.split(" ");
+        System.out.println(Arrays.toString(ans));
+//        String result =
+    }
+
+}
