3312e45b94ff5a46bc7de0c98218efded7cd6982
WebWithAstha <lodhiastha85@gmail.com>
2024-12-19 23:04:20 +0530

    obj of different class

---
+++ src/oops/CreateStudents.java
@@ -0,0 +1,10 @@
+package oops;
+
+public class CreateStudents {
+
+    public static void main(String[] args) {
+        Student s2 = new Student("Devraj",21);
+        System.out.println(s2);
+    }
+
+}
