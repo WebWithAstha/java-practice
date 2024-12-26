927c358779ee4a1aa6f27affeb3f2ed9877ffb6e
WebWithAstha <lodhiastha85@gmail.com>
2024-12-26 23:30:11 +0530

    path changed

--- src/oops/designProblems/CabService.java
+++ src/oops/designProblems/CabService.java
@@ -1,4 +1,4 @@
-package oops;
+package oops.designProblems;
 
 import java.util.Scanner;
 
--- src/oops/designProblems/RailwayTicket.java
+++ src/oops/designProblems/RailwayTicket.java
@@ -1,4 +1,4 @@
-package oops;
+package oops.designProblems;
 
 import java.io.BufferedReader;
 import java.io.IOException;
